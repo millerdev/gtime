fed340ff9b0c683b8a2b3f9f6b3ed494037cf1ef
Daniel Miller <dmiller@dimagi.com>
2018-10-24 13:21:28 +0000

    Add limit and average to client; gevent 1.3 times

--- client.py
+++ client.py
@@ -34,10 +34,20 @@ def main():
     parser.add_argument("url", help="Server URL")
     parser.add_argument("-t", "--timeout", default=5, type=int,
         help="Timeout in seconds [5]")
+    parser.add_argument("-n", "--limit", default=20, type=int,
+        help="Limit the number of batches to send [20]")
     args = parser.parse_args()
-    url = args.url
-    timeout = args.timeout
+    stats = []
 
+    try:
+        run(args.url, args.timeout, args.limit, stats)
+    except KeyboardInterrupt:
+        pass
+    if stats:
+        avg = sum(iter(stats)) / float(len(stats))
+        print("average success: {:.2f}".format(avg))
+
+def run(url, timeout, limit, stats):
     def exception_handler(request, err):
         add_error(type(err).__name__)
 
@@ -48,12 +58,18 @@ def main():
             record = errors[name] = {"name": name, "count": 0}
         record["count"] += 1
 
+    i = 0
     adj = 0
     concurrency = 32
-    while True:
+    while i < limit:
+        i += 1
         errors = new_errors()
         start = datetime.now()
-        print("{} request {} -> ".format(start, concurrency), end="", flush=1)
+        print(
+            "{} {} request {} -> ".format(i, start, concurrency),
+            end="",
+            flush=1,
+        )
         rs = grequests.map(
             (grequests.get(url, timeout=timeout) for x in range(concurrency)),
             exception_handler=exception_handler,
@@ -71,27 +87,26 @@ def main():
             else:
                 success += 1
             resp.close()
+        stats.append(success)
         if failed:
-            fail_msg = " ({})".format(" ".join(
+            adj = max(int(failed / 2.), 1)
+            if adj >= concurrency:
+                adj = concurrency - 1
+            concurrency -= adj
+            fail_msg = " (-{}: {})".format(adj, " ".join(
                 "{name}={count}".format(**err)
                 for err in sorted(errors.values(), key=itemgetter("name"))
                 if err["count"]
             ))
         else:
-            fail_msg = ""
-        print("{} responses in {}{}".format(success, elapsed, fail_msg))
-        if not failed:
             if adj:
+                fail_msg = " (+{})".format(adj)
                 concurrency += adj
                 adj *= 2
             else:
+                fail_msg = " (+{})".format(concurrency)
                 concurrency *= 2
-        else:
-            adj = max(int(failed / 2.), 1)
-            concurrency -= adj
-            if concurrency < 1:
-                concurrency = 1
-                adj = 0
+        print("{} responses in {}{}".format(success, elapsed, fail_msg))
 
 def new_errors():
     return {
